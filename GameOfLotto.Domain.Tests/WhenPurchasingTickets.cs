3fc876c735377c0247c2a02afefa4da24a07d058
Anth Richardson <anthrblahblah@gmail.com>
2025-07-01 20:24:25 +0100

    Refactored WhenPurchasingTickets class

--- GameOfLotto.Domain.Tests/WhenPurchasingTickets.cs
+++ GameOfLotto.Domain.Tests/WhenPurchasingTickets.cs
@@ -3,20 +3,26 @@ namespace GameOfLotto.Domain.Tests;
 
 public class WhenPurchasingTickets
 {
+    private readonly Player _player;
+    private readonly InMemoryPlayerRepository _playerRepository;
+    private readonly TicketOffice _ticketOffice;
+
+    public WhenPurchasingTickets()
+    {
+        _player = new Player("Player 1", Guid.NewGuid());
+        _playerRepository = new InMemoryPlayerRepository();
+        _playerRepository.Save(_player);
+        _ticketOffice = new TicketOffice(_playerRepository);
+    }
+
     [Fact]
     public void The_player_will_receive_a_single_ticket()
     {
-        // Arrange
-        var player = new Player("Player 1", Guid.NewGuid());
-        var playerRepository = new InMemoryPlayerRepository();
-        playerRepository.Save(player);
-        var ticketOffice = new TicketOffice(playerRepository);
-
         // Act
-        ticketOffice.Purchase(player.Id, 1);
+        _ticketOffice.Purchase(_player.Id, 1);
         
         // Assert
-        var persistedPlayer = playerRepository.Saved[0];
+        var persistedPlayer = _playerRepository.Saved[0];
         Assert.Single(persistedPlayer.Tickets);
     }
 }

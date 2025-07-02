bf3b886f6f9dfc96eab7de4656fefdef6eb0a61f
Anth Richardson <anthrblahblah@gmail.com>
2025-07-02 21:32:54 +0100

    Implements the core game logic
    Introduces the game mechanics, including ticket drawing and prize awarding.

---
+++ GameOfLotto.Domain.Tests/WhenRunningTheGame.cs
@@ -0,0 +1,30 @@
+﻿namespace GameOfLotto.Domain.Tests;
+
+public class WhenRunningTheGame
+{
+    [Fact]
+    public void The_first_ticket_drawn_will_be_awarded_the_grand_prize()
+    {
+        // Arrange
+        var game = new Game();
+        var playerRepo = new InMemoryPlayerRepository();
+        var playerManifest = new PlayerManifest(playerRepo, game);
+        var ticketOffice = new TicketOffice(playerRepo);
+        var players = playerManifest.AddCpuPlayers("Player", 10).ToArray();
+        
+        foreach (var player in players)
+        {
+            ticketOffice.Purchase(player.Id, 2);
+        }
+
+        var seed = 1234;
+        var seededWinningTicketNumber = 8;
+        var winner = players[seededWinningTicketNumber / 2 - 1]; 
+
+        // Act
+        var result = game.Run(playerRepo, seed);
+        
+        // Assert
+        Assert.Equivalent(new[] { winner.Id }, result.GrandPrize.Winners.Select(p => p.Id));
+    }
+}
--- GameOfLotto.Domain/Game.cs
+++ GameOfLotto.Domain/Game.cs
@@ -4,8 +4,23 @@ public class Game(Guid id = default)
 {
     public Guid Id { get; private set; } = id;
 
+    public Result Run(IPlayerRepository playerRepository, int seed)
+    {
+        var players = playerRepository.Get().Where(p => p.GameIds.Contains(Id)).ToArray();
+        var tickets = players.SelectMany(p => p.Tickets).ToArray();
+        var rng = new Random(seed);
+        var grandPrize = rng.Next(1, tickets.Length);
+        var winningTicket = tickets.ElementAt(grandPrize - 1);
+        var grandPrizeWinner = players.First(p => p.Tickets.Contains(winningTicket));
+        return new Result(new Prize([grandPrizeWinner], new Amount("USD", 0)));
+    }
+
     public IList<object> GetPlayers()
     {
         return [];
     }
+
+    public record Result(Prize GrandPrize);
+
+    public record Prize(Player[] Winners, Amount Value);
 }

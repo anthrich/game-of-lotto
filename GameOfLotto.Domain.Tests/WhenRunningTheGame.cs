4d772309471a5437dbfe21e8221bee978c7c502b
Anth Richardson <anthrblahblah@gmail.com>
2025-07-02 22:24:09 +0100

    Implements second tier prize distribution
    Extends the game logic to include a second tier prize pool.
    
    Calculates the second tier prize value and distributes it among two randomly selected winners.

--- GameOfLotto.Domain.Tests/WhenRunningTheGame.cs
+++ GameOfLotto.Domain.Tests/WhenRunningTheGame.cs
@@ -7,9 +7,11 @@ public class WhenRunningTheGame
     private readonly PlayerManifest _playerManifest;
     private readonly TicketOffice _ticketOffice;
     private readonly Player[] _players;
+    private readonly Ticket[] _tickets;
     private const int Seed = 1234;
     // given the seed, the first random of 1-20 tickets will be 8
-    private const int SeededWinningTicketNumber = 8;
+    private readonly int _seededWinningTicketNumber;
+    private readonly int[] _seededSecondTierWinningTicketNumbers = [0, 0];
 
     public WhenRunningTheGame()
     {
@@ -18,22 +20,28 @@ public class WhenRunningTheGame
         _playerManifest = new PlayerManifest(_playerRepo, _game);
         _ticketOffice = new TicketOffice(_playerRepo);
         _players = _playerManifest.AddCpuPlayers("Player", 10).ToArray();
+        
         foreach (var player in _players)
         {
             _ticketOffice.Purchase(player.Id, 2);
         }
+
+        _players = _playerRepo.Get().ToArray();
+        _tickets = _players.SelectMany(p => p.Tickets).ToArray();
+        var random = new Random(Seed);
+        _seededWinningTicketNumber = random.Next(1, 20);
+        _seededSecondTierWinningTicketNumbers[0] = random.Next(1, 19);
+        _seededSecondTierWinningTicketNumbers[1] = random.Next(1, 18);
     }
 
     [Fact]
-    public void The_first_ticket_drawn_will_be_awarded_the_grand_prize()
+    public void The_first_ticket_drawn_will_award_the_grand_prize_to_its_owner()
     {
-        // Arrange
-        var winner = _players[SeededWinningTicketNumber / 2 - 1]; 
-
         // Act
         var result = _game.Run(_playerRepo, Seed);
         
         // Assert
+        var winner = _players[_seededWinningTicketNumber / 2 - 1]; 
         Assert.Equivalent(new[] { winner.Id }, result.GrandPrize.Winners.Select(p => p.Id));
     }
 
@@ -45,5 +53,30 @@ public class WhenRunningTheGame
         
         // Assert
         Assert.Equal(new Amount("USD", 10), result.GrandPrize.Value);
+    }
+
+    [Fact]
+    public void The_next_2_tickets_will_be_awarded_the_second_tier_prizes()
+    {
+        
+        // Act
+        var result = _game.Run(_playerRepo, Seed);
+        
+        // Assert
+        var winningTicketOne = _tickets[_seededSecondTierWinningTicketNumbers[0] - 1];
+        var winningTicketTwo = _tickets[_seededSecondTierWinningTicketNumbers[1] - 1];
+        var winnerOne = _players.First(p => p.Tickets.Any(t => t.Id == winningTicketOne.Id));
+        var winnerTwo = _players.First(p => p.Tickets.Any(t => t.Id == winningTicketTwo.Id));
+        Assert.Equivalent(new[] { winnerOne.Id, winnerTwo.Id }, result.SecondTier.Winners.Select(p => p.Id));
+    }
+    
+    [Fact]
+    public void The_second_tier_prize_will_be_30_percent_of_the_ticket_revenue_split_between_two()
+    {
+        // Act
+        var result = _game.Run(_playerRepo, Seed);
+        
+        // Assert
+        Assert.Equal(new Amount("USD", 3), result.SecondTier.Value);
     }
 }
--- GameOfLotto.Domain/Game.cs
+++ GameOfLotto.Domain/Game.cs
@@ -7,13 +7,35 @@ public class Game(Guid id = default)
     public Result Run(IPlayerRepository playerRepository, int seed)
     {
         var players = playerRepository.Get().Where(p => p.GameIds.Contains(Id)).ToArray();
-        var tickets = players.SelectMany(p => p.Tickets).ToArray();
+        var allTickets = players.SelectMany(p => p.Tickets).ToList();
         var rng = new Random(seed);
-        var grandPrize = rng.Next(1, tickets.Length);
-        var winningTicket = tickets.ElementAt(grandPrize - 1);
-        var grandPrizeWinner = players.First(p => p.Tickets.Contains(winningTicket));
-        var grandPrizeAmount = tickets.Sum(t => t.Cost.Value) * 0.5m;
-        return new Result(new Prize([grandPrizeWinner], new Amount("USD", grandPrizeAmount)));
+        var grandPrize = GetPrize(rng, allTickets, players);
+        var grandPrizeAmount = allTickets.Sum(t => t.Cost.Value) * 0.5m;
+        var secondTier = Math.Round(allTickets.Count * 0.1m);
+        var ticketsInPlay = allTickets.Except([grandPrize.Ticket]).ToList();
+        var secondTierWinners = new List<Player>();
+        
+        for (var i = 0; i < secondTier; i++)
+        {
+            var prize = GetPrize(rng, ticketsInPlay, players);
+            ticketsInPlay.Remove(prize.Ticket);
+            secondTierWinners.Add(prize.Player);
+        }
+        
+        var secondTierAmount = allTickets.Sum(t => t.Cost.Value) * 0.3m / secondTier;
+        
+        return new Result(
+            new Prize([grandPrize.Player], new Amount("USD", grandPrizeAmount)),
+            new Prize(secondTierWinners.ToArray(), new Amount("USD", secondTierAmount))
+        );
+    }
+
+    private static PrizeAward GetPrize(Random rng, List<Ticket> tickets, Player[] players)
+    {
+        var prize = rng.Next(1, tickets.Count);
+        var prizeTicket = tickets.ElementAt(prize - 1);
+        var prizeWinner = players.First(p => p.Tickets.Contains(prizeTicket));
+        return new PrizeAward(prizeWinner, prizeTicket);
     }
 
     public IList<object> GetPlayers()
@@ -21,7 +43,9 @@ public class Game(Guid id = default)
         return [];
     }
 
-    public record Result(Prize GrandPrize);
+    public record Result(Prize GrandPrize, Prize SecondTier);
 
     public record Prize(Player[] Winners, Amount Value);
+    
+    private record PrizeAward(Player Player, Ticket Ticket);
 }
